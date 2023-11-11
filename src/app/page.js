51e235804c5b69e25493a8f09c8b5d69834ecdcb
Senthil Arivudainambi <me@sent-hil.com>
2023-11-10 17:56:42 -0800

    move Flyout to own file

--- src/app/page.js
+++ src/app/page.js
@@ -2,78 +2,7 @@
 
 import { useEffect, useRef, Fragment, useState } from "react";
 import Script from "next/script";
-import { Dialog, Transition } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
-
-function FlyOut({ flyOutOpen, setFlyOutOpen }) {
-  return (
-    <Transition.Root show={flyOutOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setFlyOutOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="fixed inset-y-0 right-0 flex max-w-full pl-10 pointer-events-none">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="relative w-screen max-w-sm pointer-events-auto">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-500"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-500"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute top-0 left-0 flex pt-4 pr-2 -ml-8 sm:-ml-10 sm:pr-4">
-                      <button
-                        type="button"
-                        className="relative text-gray-300 rounded-md hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                        onClick={() => setFlyOutOpen(false)}
-                      >
-                        <span className="absolute -inset-2.5" />
-                        <span className="sr-only">Close panel</span>
-                        <XMarkIcon className="w-6 h-6" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  <div className="flex flex-col h-full py-6 overflow-y-scroll bg-white shadow-xl">
-                    <div className="px-4 sm:px-6">
-                      <Dialog.Title>
-                        <p className="text-xl font-bold">Data</p>
-                      </Dialog.Title>
-                    </div>
-                    <div className="relative flex-1 px-4 mt-6 sm:px-6">
-                      {/* Your content */}
-                    </div>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
-  );
-}
+import FlyOut from "./flyout";
 
 const DATA = Array.from(Array(100)).map((_, i) => ({
   text: "Hello",
