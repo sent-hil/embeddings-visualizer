6aaee73be12ba8f36702890c9e40cb4226242aa1
Senthil Arivudainambi <me@sent-hil.com>
2024-04-27 11:08:10 -0700

    switch to content editable

--- src/app/default_data.js
+++ src/app/default_data.js
@@ -11,15 +11,15 @@ const DefaultData = {
     "Mountain",
   ],
   data: [
-    { i: 0, x: 1, y: 2, text: "Joyful" },
-    { i: 1, x: 2, y: 3, text: "Melancholic" },
-    { i: 2, x: 3, y: 4, text: "Surprised" },
-    { i: 3, x: 4, y: 5, text: "Innovation" },
-    { i: 4, x: 5, y: 6, text: "Cybersecurity" },
-    { i: 5, x: 6, y: 7, text: "Artificial Intelligence" },
-    { i: 6, x: 7, y: 8, text: "Forest" },
-    { i: 7, x: 8, y: 9, text: "Ocean" },
-    { i: 8, x: 9, y: 10, text: "Mountain" },
+    {i: 0, x: '0.0928', y: '-0.279', text: 'Joyful'},
+    {i: 1, x: '0.125', y: '-0.268', text: 'Melancholic'},
+    {i: 2, x: '0.0110', y: '-0.225', text: 'Surprised'},
+    {i: 3, x: '-0.236', y: '0.00875', text: 'Innovation'},
+    {i: 4, x: '-0.317', y: '0.120', text: 'Cybersecurity'},
+    {i: 5, x: '-0.272', y: '0.0638', text: 'Artificial Intelligence'},
+    {i: 6, x: '0.182', y: '0.209', text: 'Forest'},
+    {i: 7, x: '0.152', y: '0.148', text: 'Ocean'},
+    {i: 8, x: '0.263', y: '0.223', text: 'Mountain'}
   ]
 }
 
--- src/app/form.js
+++ src/app/form.js
@@ -48,19 +48,22 @@ const Form = ({
         <div className="sm:col-span-2">
           <label
             htmlFor="message"
-            className="block text-sm font-semibold leading-6 text-gray-900"
+            className="flex space-x-1 text-sm font-semibold leading-6 text-gray-900"
           >
-            Items
+            <p>Items</p><span className="italic font-normal">- Separate by new lines</span>
           </label>
           <div className="mt-2.5">
-            <textarea
+            <div
+              contenteditable="true"
               name="message"
               id="message"
               rows={4}
               value={input}
-              onChange={(e) => setInput(e.target.value)}
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
+              onInput={(e) => setInput(e.currentTarget.textContent)}
+              className="block w-full min-h-fit rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 whitespace-pre"
+            >
+              {input}
+              </div>
           </div>
           {
             Array(providerCount).fill().map((_, i) => (
@@ -73,9 +76,6 @@ const Form = ({
               />
             ))
           }
-          <div className="mt-2">
-            <a href="#" className="text-sm" onClick={() => setProviderCount((p) => p+1)}>Add new Provider</a>
-          </div>
         </div>
       </div>
       <div className="mt-5">
